ab4d8b73c5af68b3be5ee6bfa80be8e29d48c9a3
BohdanHromchak <gromchakbogdan@gmail.com>
2023-03-25 23:57:30 +0200

    Update Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,20 +1,14 @@
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { Container } from '@mui/material';
 
 const Contacts = () => {
   return (
     <>
       <ContactForm />
 
-      {/* <Container sx={{  display: 'flex', alignItems: 'center',
-          gap: '1rem', 
-          flexDirection: 'column',
-          justifyContent: 'center',}}> */}
-        <Filter />
-        <ContactList />
-      {/* </Container> */}
+      <Filter />
+      <ContactList />
     </>
   );
 };
